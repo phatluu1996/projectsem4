919a7243aa2d24e1154f5a653d9db0907692113a
Pham Xuan Bach <53576898+bachpham995@users.noreply.github.com>
2022-06-23 00:37:46 +0700

    Initial backend
    Initial backend SpringBoot

---
+++ backend/src/test/java/com/hospitalbooking/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospitalbooking.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
