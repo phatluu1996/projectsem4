cbd8e6593a458efd9494b60f7e5ec0c2d5c534d2
phatluu1996 <53576411+phatluu1996@users.noreply.github.com>
2022-06-26 00:58:10 +0700

    Update Side bar

--- teal/src/adminroute.jsx
+++ teal/src/adminroute.jsx
@@ -2,11 +2,14 @@ import React, { Component } from "react";
 import { Route, Redirect } from 'react-router-dom';
 import Header from "./admin/components/header";
 import '../src/admin/assets/css/style.css';
+import Sidebar from "./admin/components/sidebar";
 
-const AdminRoute = ({component: Component, ...rest}) => {
+const AdminRoute = ({ component: Component, ...rest }) => {
     return (<div id="admin">
+        <Route render={(props) => <Sidebar {...props} />} />
         <Route render={(props) => <Header {...props} />} />
-        <Route {...rest} render={props => ( <Component {...props} />)}/>
+        <Route {...rest} render={props => (<Component {...props} />)} />
+        <div className="sidebar-overlay" data-reff />
     </div>
     );
 };
--- teal/src/businessroute.jsx
+++ teal/src/businessroute.jsx
@@ -4,10 +4,10 @@ import Footer from "./user/components/footer";
 import Header from "./user/components/header";
 import '../src/assets/css/style.css';
 
-const BusinessRoute = ({component: Component, ...rest}) => {
-    return (<div id="business">
+const BusinessRoute = ({ component: Component, ...rest }) => {
+    return (<div id="business">        
         <Route render={(props) => <Header {...props} />} />
-        <Route {...rest} render={props => ( <Component {...props} />)}/>
+        <Route {...rest} render={props => (<Component {...props} />)} />
         <Route render={(props) => <Footer {...props} />} />
     </div>
     );
