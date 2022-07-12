5fd1fbb52a748e77b6caf5278430e09394b58746
phatluu1996 <53576411+phatluu1996@users.noreply.github.com>
2022-07-13 00:52:47 +0700

    Update Progresss

--- backend/src/main/java/com/hospitalbooking/backend/BackendApplication.java
+++ backend/src/main/java/com/hospitalbooking/backend/BackendApplication.java
@@ -1,7 +1,12 @@
 package com.hospitalbooking.backend;
 
+import com.hospitalbooking.backend.models.Department;
+import com.hospitalbooking.backend.repository.DepartmentRepos;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -10,4 +15,14 @@ public class BackendApplication {
         SpringApplication.run(BackendApplication.class, args);
     }
 
+    @Autowired
+    DepartmentRepos departmentRepos;
+
+//    @Bean
+//    InitializingBean sendDatabase() {
+//        return () -> {
+//            departmentRepos.save(new Department((long)1,"Dentist", "Dentists", true, false));
+//            departmentRepos.save(new Department((long)2,"Cardiologist", "Cardiologist", true, false));
+//        };
+//    }
 }
